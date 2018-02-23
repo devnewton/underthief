66e1eeb037054ea3c4be8c129d1b8c2136560fef
devnewton <devnewton@bci.im>
2018-02-23 22:07:57 +0100

    add stupid unblock cpu logic

--- app/ia/CPU.ts
+++ app/ia/CPU.ts
@@ -11,25 +11,60 @@ export class CPU {
 
     think() {
         this.controls.reset();
+        if (!this.waiting()) {
+            let underwear = this.underwears.getClosestTo(this.me, u => u.body.enable);
+            let capturePoint = this.capturePoints.getClosestTo(this.me, u => u.body.enable);
+            if (underwear && capturePoint) {
+                if (this.me.game.physics.arcade.overlap(this.me, this.underwears)) {
+                    this.waitUntil = this.me.game.time.time + 500;
+                } else {
+                    this.pushUnderwear(underwear, capturePoint);
+
+                }
+            }
+        }
+    }
+    pushUnderwear(underwear, capturePoint) {
+        if (underwear.body.blocked.up && underwear.body.blocked.left) {
+            this.unblockTopLeftUnderwear(underwear);
+        } else if (underwear.body.blocked.up && underwear.body.blocked.right) {
+            this.unblockTopRightUnderwear(underwear);
+        } if (underwear.body.blocked.down && underwear.body.blocked.left) {
+            this.unblockBottomLeftUnderwear(underwear);
+        } else if (underwear.body.blocked.down && underwear.body.blocked.right) {
+            this.unblockBottomRightUnderwear(underwear);
+        } else {
+            this.pushUnderwearToCapturePoint(underwear, capturePoint);
+        }
+    }
+
+    pushUnderwearToCapturePoint(underwear, capturePoint) {
+        let ucVector = new Phaser.Point(underwear.body.x - capturePoint.body.x, underwear.body.y - capturePoint.body.y);
+        ucVector.setMagnitude(underwear.body.radius).add(underwear.body.x, underwear.body.y);
+        this.moveToXY(ucVector.x, ucVector.y);
+    }
+
+    unblockTopLeftUnderwear(underwear) {
+        this.moveToXY(underwear.left, underwear.top);
+    }
+    unblockTopRightUnderwear(underwear) {
+        this.moveToXY(underwear.right, underwear.top);
+    }
+    unblockBottomLeftUnderwear(underwear) {
+        this.moveToXY(underwear.left, underwear.bottom);
+    }
+    unblockBottomRightUnderwear(underwear) {
+        this.moveToXY(underwear.right, underwear.bottom);
+    }
+    waiting(): boolean {
         if (this.waitUntil) {
             if (this.me.game.time.time < this.waitUntil) {
-                return;
+                return true;
             } else {
                 this.waitUntil = null;
             }
         }
-        let underwear = this.underwears.getClosestTo(this.me, u => u.body.enable);
-        let capturePoint = this.capturePoints.getClosestTo(this.me, u => u.body.enable);
-        if (underwear && capturePoint) {
-            if (this.me.game.physics.arcade.overlap(this.me, this.underwears)) {
-                this.waitUntil = this.me.game.time.time + 500;
-            } else {
-                let ucVector = new Phaser.Point(underwear.body.x - capturePoint.body.x, underwear.body.y - capturePoint.body.y);
-                ucVector.setMagnitude(underwear.body.radius)
-                    .add(underwear.body.x, underwear.body.y);
-                this.moveToXY(ucVector.x, ucVector.y);
-            }
-        }
+        return false;
     }
 
     moveToXY(x: number, y: number) {
